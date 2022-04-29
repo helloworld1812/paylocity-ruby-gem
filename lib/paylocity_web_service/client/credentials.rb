cabf882633ee77e6c86345e80c348c2ae6c05d0c
Ryan Lyu <xiaorong.ruby@gmail.com>
2022-04-29 15:20:18 +0800

    bugfix: add payload for Obtain new client secret.

--- lib/paylocity_web_service/client/credentials.rb
+++ lib/paylocity_web_service/client/credentials.rb
@@ -1,8 +1,8 @@
 module PaylocityWebService
   class Client
     module Credentials
-      def create_client_credential
-        post("/api/v2/credentials/secrets")
+      def create_client_credential(options={})
+        post("/api/v2/credentials/secrets", options)
       end
     end
   end
